ec2704f15c3aeb48c64cd8af7a11ea936850d271
Ray Bell <ray.bell@dtn.com>
2024-05-28 22:05:33 -0400

    commit

--- pull_issues.zsh
+++ pull_issues.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
+#
+# ./pull_issues.zsh
 
 REPO="raybellwaves/llm-issue-explorer"
-
-issue_count=$(gh issue list --repo $REPO --json number | jq '. | length')
-
-for ((i=1; i<=issue_count; i++)); do
-  gh issue view --repo $REPO $i --json assignees,author,body,closed,closedAt,comments,createdAt,id,labels,milestone,number,projectCards,projectItems,reactionGroups,state,title,updatedAt,url > "issue_detail_$i.json"
+issue_numbers=($(gh issue list --repo $REPO --state open --limit 1000 --json number | jq -r '.[].number'))
+for issue_number in "${issue_numbers[@]}"; do
+    echo "--$issue_number--"
+    gh issue view --repo $REPO $issue_number --json assignees,author,body,closed,closedAt,comments,createdAt,id,labels,milestone,number,projectCards,projectItems,reactionGroups,state,title,updatedAt,url >issue_detail_$issue_number.json
 done
--- utils.zsh
+++ utils.zsh
@@ -1,4 +1,15 @@
 #!/bin/zsh
+#
+#./utils.zsh
 
-issue_body=$(llm -m mistral-7b-instruct-v0 'create a fake github issue')
-gh issue create --repo raybellwaves/llm-issue-explorer --title "Generated Issue" --body "$issue_body"
+# REPO="raybellwaves/llm-issue-explorer"
+# Create a fake issue
+# issue_body=$(llm -m mistral-7b-instruct-v0 'create a fake github issue')
+# gh issue create --repo raybellwaves/llm-issue-explorer --title "Generated Issue" --body "$issue_body"
+
+issue_count=$(gh issue list --repo raybellwaves/llm-issue-explorer --json number | jq '. | length')
+for i in $(seq 1 30); do
+    j=$((i + issue_count))
+    gh issue create --repo raybellwaves/llm-issue-explorer --title "Issue ${j}" --body "Body ${j}"
+    sleep 5
+done
