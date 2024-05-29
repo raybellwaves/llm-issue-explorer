83606a4bc567a25bf0ca42414ac9104768796024
Ray Bell <ray.bell@dtn.com>
2024-05-29 15:18:07 -0400

    commit

--- utils.py
+++ utils.py
@@ -50,7 +50,68 @@ def join_csvs():
     df.to_csv("issue_details_with_posters.csv")
 
 
+def upload_csv_to_hugging_face_hub():
+    from huggingface_hub import HfApi, HfFolder
+
+    token = os.environ["HF_WRITE_TOKEN"]
+    HfFolder.save_token(token)
+    username = "raybellwaves"
+    repo_name = "raybellwaves-llm-issue-explorer-issues"
+    file_path = "issue_details_with_posters.csv"
+
+    api = HfApi()
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path,
+        repo_id=f"{username}/{repo_name}",
+        repo_type="dataset",
+        token=token,
+    )
+
+
+def read_csv_from_hugging_face_hub():
+    from datasets import load_dataset
+
+    dataset = load_dataset("raybellwaves/raybellwaves-llm-issue-explorer-issues")
+    df = dataset["train"].to_pandas()
+    return df
+
+
+def chat_to_dataset_using_langchain():
+    from langchain_experimental.agents.agent_toolkits import (
+        create_pandas_dataframe_agent,
+    )
+    from langchain_openai import OpenAI
+
+    df = read_csv_from_hugging_face_hub()
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True)
+    agent.invoke("how many rows are there?")["output"]
+    # agent.invoke("How many different companies created an issue?")["output"]
+    # agent.invoke("Which user created the most issues?")["output"]
+    # agent.invoke(
+    #     "Using the reactionGroups column, which issue has the most THUMBS_UP?"
+    # )["output"]
+
+
+def chat_to_dataset_using_pandasai():
+    from pandasai import SmartDataframe
+    from pandasai.llm import OpenAI
+
+    llm = OpenAI(api_token=os.environ["OPENAI_API_KEY"])
+    df = read_csv_from_hugging_face_hub()
+    smart_df = SmartDataframe(df, config={"llm": llm})
+    smart_df.chat("How many rows are there?")
+    smart_df.chat("How many different companies created an issue?")
+    smart_df.chat("Which user created the most issues?")
+    smart_df.chat(
+        "Using the reactionGroups column, which issue has the most THUMBS_UP?"
+    )
+
+
 if __name__ == "__main__":
     # concat_issues()
     # concat_posters()
-    join_csvs()
+    # join_csvs()
+    # upload_csv_to_hugging_face_hub()
+    chat_to_dataset_using_langchain()
+    # pass
